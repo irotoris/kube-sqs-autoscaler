8d1a6ba49db1a4e82411a062ff56b5f5f995769e
irotoris <shiroto00@yahoo.co.jp>
2020-11-28 23:09:10 +0900

    local kube config

--- scale/scale.go
+++ scale/scale.go
@@ -2,6 +2,7 @@ package scale
 
 import (
 	"context"
+	"os"
 
 	"github.com/pkg/errors"
 
@@ -13,6 +14,10 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+var (
+	kubeConfigPath string
+)
+
 type PodAutoScaler struct {
 	Client     typedappv1.DeploymentInterface
 	Max        int
@@ -22,9 +27,10 @@ type PodAutoScaler struct {
 }
 
 func NewPodAutoScaler(kubernetesDeploymentName string, kubernetesNamespace string, max int, min int) *PodAutoScaler {
-	config, err := clientcmd.BuildConfigFromFlags("", "")
+	kubeConfigPath = os.Getenv("KUBE_CONFIG_PATH")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
 	if err != nil {
-		panic("Failed to configure incluster config")
+		panic("Failed to configure incluster or local config")
 	}
 
 	k8sClient, err := kubernetes.NewForConfig(config)
